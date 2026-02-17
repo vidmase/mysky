9e3d583832536e66434a2a2e37fe32f5a2d2ccd6
Vidmantas D <vidmase@gmail.com>
2026-02-17 20:27:13 +0000

    Update Next.js to 16.1.6 to fix CVE-2025-66478

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -19,19 +17,7 @@ const nextConfig = {
       },
     ],
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Ensure proper module resolution
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname),
-    }
-    return config
-  },
+
 }
 
 module.exports = nextConfig
